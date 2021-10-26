52b3e82eeea1815b4b8032a1cf494d330deb6e4c
Pritish Joshi <rjpritish@gmail.com>
2021-10-26 10:10:55 +0530

    Add Scripts

---
+++ debloat_rmui2.0-RMX3151.sh
@@ -0,0 +1,87 @@
+echo "Running REALME 8i debloat script by Pritish"
+
+echo "Uninstalling stk"
+adb shell pm uninstall -k --user 0 com.android.stk
+
+echo "Uninstalling video"
+adb shell pm uninstall -k --user 0 com.coloros.video
+
+echo "Uninstalling browser"
+adb shell pm uninstall -k --user 0 com.heytap.browser
+
+echo "Uninstalling google keep"
+adb shell pm uninstall -k --user 0 com.google.android.keep
+
+echo "Uninstalling youtube"
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+
+echo "Uninstalling google news"
+adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
+
+echo "Disabling heytap cloud"
+adb shell pm disable-user --user 0 com.heytap.cloud
+
+echo "Uninstalling gamecentre"
+adb shell pm uninstall -k --user 0 com.nearme.gamecenter
+
+echo "Uninstalling phonemanager"
+adb shell pm uninstall -k --user 0 com.coloros.phonemanager
+
+echo "Uninstalling google assistant"
+adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant
+
+echo "Uninstalling facebook appmanager"
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+
+echo "Uninstalling facebook services"
+adb shell pm uninstall -k --user 0 com.facebook.services
+
+echo "Uninstalling coloros calculator"
+adb shell pm uninstall -k --user 0 com.coloros.calculator
+
+echo "Uninstalling finshell"
+adb shell pm uninstall -k --user 0 com.finshell.fin
+
+echo "Uninstalling clone phone"
+adb shell pm uninstall -k --user 0 com.coloros.backuprestore
+
+echo "Uninstalling hot apps"
+adb shell pm uninstall -k --user 0 com.oppo.cs
+
+echo "Uninstalling kids space"
+adb shell pm uninstall -k --user 0 com.coloros.childrenspace
+
+echo "Uninstalling lockscreen magazine"
+adb shell pm uninstall -k --user 0 com.heytap.pictorial
+
+echo "Uninstalling my realme"
+adb shell pm uninstall -k --user 0 com.heytap.usercenter
+
+echo "Uninstalling sound recorder"
+adb shell pm uninstall -k --user 0 com.coloros.soundrecorder
+
+echo "Disabling themestore"
+adb shell pm disable-user --user 0 com.heytap.themestore
+
+echo "Uninstalling weather"
+adb shell pm uninstall -k --user 0 com.coloros.weather2
+
+echo "Uninstalling compass"
+adb shell pm uninstall -k --user 0 com.coloros.compass2
+
+echo "Uninstalling oroaming"
+adb shell pm uninstall -k --user 0 com.redteamobile.roaming
+
+echo "Uninstalling filemanager"
+adb shell pm uninstall -k --user 0 com.coloros.filemanager
+
+echo "Disabling clock"
+adb shell pm disable-user --user 0 com.coloros.alarmclock
+
+echo "Uninstalling music"
+adb shell pm uninstall -k --user 0 com.heytap.music
+
+echo "Uninstalling google lens"
+adb shell pm uninstall -k --user 0 com.google.ar.lens
+
+echo "Done!"
---
+++ rebloat_rmui2.0-RMX3151.sh
@@ -0,0 +1,87 @@
+echo "Running REALME 8i rebloat script by Pritish"
+
+echo "Reinstalling stk"
+adb shell cmd package install-existing --user 0 com.android.stk
+
+echo "Reinstalling video"
+adb shell cmd package install-existing --user 0 com.coloros.video
+
+echo "Reinstalling browser"
+adb shell cmd package install-existing --user 0 com.heytap.browser
+
+echo "Reinstalling google keep"
+adb shell cmd package install-existing --user 0 com.google.android.keep
+
+echo "Reinstalling youtube"
+adb shell cmd package install-existing --user 0 com.google.android.youtube
+
+echo "Reinstalling google news"
+adb shell cmd package install-existing --user 0 com.google.android.apps.magazines
+
+echo "Enabling heytap cloud"
+adb shell pm enable-user --user 0 com.heytap.cloud
+
+echo "Reinstalling gamecentre"
+adb shell cmd package install-existing --user 0 com.nearme.gamecenter
+
+echo "Reinstalling phonemanager"
+adb shell cmd package install-existing --user 0 com.coloros.phonemanager
+
+echo "Reinstalling google assistant"
+adb shell cmd package install-existing --user 0 com.google.android.apps.googleassistant
+
+echo "Reinstalling facebook appmanager"
+adb shell cmd package install-existing --user 0 com.facebook.appmanager
+
+echo "Reinstalling facebook services"
+adb shell cmd package install-existing --user 0 com.facebook.services
+
+echo "Reinstalling coloros calculator"
+adb shell cmd package install-existing --user 0 com.coloros.calculator
+
+echo "Reinstalling finshell"
+adb shell cmd package install-existing --user 0 com.finshell.fin
+
+echo "Reinstalling clone phone"
+adb shell cmd package install-existing --user 0 com.coloros.backuprestore
+
+echo "Reinstalling hot apps"
+adb shell cmd package install-existing --user 0 com.oppo.cs
+
+echo "Reinstalling kids space"
+adb shell cmd package install-existing --user 0 com.coloros.childrenspace
+
+echo "Reinstalling lockscreen magazine"
+adb shell cmd package install-existing --user 0 com.heytap.pictorial
+
+echo "Reinstalling my realme"
+adb shell cmd package install-existing --user 0 com.heytap.usercenter
+
+echo "Reinstalling sound recorder"
+adb shell cmd package install-existing --user 0 com.coloros.soundrecorder
+
+echo "Enabling themestore"
+adb shell pm enable-user --user 0 com.heytap.themestore
+
+echo "Reinstalling weather"
+adb shell cmd package install-existing --user 0 com.coloros.weather2
+
+echo "Reinstalling compass"
+adb shell cmd package install-existing --user 0 com.coloros.compass2
+
+echo "Reinstalling oroaming"
+adb shell cmd package install-existing --user 0 com.redteamobile.roaming
+
+echo "Reinstalling filemanager"
+adb shell cmd package install-existing --user 0 com.coloros.filemanager
+
+echo "Enabling clock"
+adb shell pm enable-user --user 0 com.coloros.alarmclock
+
+echo "Reinstalling music"
+adb shell cmd package install-existing --user 0 com.heytap.music
+
+echo "Reinstalling google lens"
+adb shell cmd package install-existing --user 0 com.google.ar.lens
+
+echo "Done!"
